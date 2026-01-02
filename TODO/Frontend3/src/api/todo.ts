4acb8b9cf6e3827ab56e0d5833c9f81d29cf8580
Nakul <nakulgite08@gmail.com>
2026-01-02 07:29:57 +0530

    api call for todos added

--- TODO/Frontend3/src/api/todo.ts
+++ TODO/Frontend3/src/api/todo.ts
@@ -5,15 +5,53 @@ dotenv.config();
 
 const BASE_URL = process.env.BASE_URL;
 
-
+const getToken = ()=>{
+    const token = localStorage.getItem('token');
+    if(!token) throw  new Error("Unauthorised user!");
+    return `Bearer ${token}`
+}
 
 //create
 const createTodo = async(title : string) : Promise<Todo> =>{
-    
+    const response = await axios.post<{todo : Todo}>(`${BASE_URL}/api/todo/`, {title}, {
+        headers : {
+            Authorization : getToken()
+        }
+    })
+
+    return response.data.todo;
 }
 
 //read
 
+const getTodos = async() : Promise<Todo[]> =>{
+    const response = await axios.get<{todos: Todo[]}>(`${BASE_URL}/api/todo/`, {
+        headers : {
+            Authorization : getToken()
+        }
+    })
+
+    return response.data.todos;
+}
+
 //update
 
+const updateTodo = async(id : string, completed : string) : Promise<Todo> =>{
+    const response = await axios.put<{todo: Todo}>(`${BASE_URL}/api/todo/${id}`, {completed}, {
+        headers : {
+            Authorization : getToken()
+        }
+    })
+
+    return response.data.todo;
+}
+
 //delete
+
+const deleteTodo = async(id : string) : Promise<void> =>{
+    await axios.delete(`${BASE_URL}/api/todo/${id}`, {
+        headers : {
+            Authorization : getToken()
+        }
+    })
+}
